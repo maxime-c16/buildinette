df6d0af657f584acdf9d7f18f249efa54b3cbcae
Melissa Cauchy <melissacauchy@MacBook-Air-de-Melissa.local>
2025-06-27 17:46:40 +0200

    added: interactive mode and cpp support as well as subdirs

--- include/default_project.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef DEFAULT_PROJECT_HPP
-
-# define DEFAULT_PROJECT_HPP
-
-// Add your declarations here
-
-#endif
--- src/default_project.cpp
+++
@@ -1,6 +0,0 @@
-#include "../include/default_project.hpp"
-
-int main(int ac, char **av)
-{
-    return (0);
-}
